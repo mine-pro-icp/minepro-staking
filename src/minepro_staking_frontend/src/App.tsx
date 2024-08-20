cab3f070b03005c92ae99ca5f6a3df2c92885af8
giangcsp <doandinhgiang99@gmail.com>
2024-08-19 21:08:20 -0400

    done with frontend

--- src/minepro_staking_frontend/src/App.tsx
+++ src/minepro_staking_frontend/src/App.tsx
@@ -1,92 +1,235 @@
 import { useEffect, useState } from 'react';
 import { minepro_staking_backend, createActor as createBackendActor } from '../declarations/minepro_staking_backend';
-import { _SERVICE as _BACKEND_SERVICE, Metadata } from '../declarations/minepro_staking_backend/minepro_staking_backend.did';
+import { _SERVICE as _BACKEND_SERVICE, Metadata, Result } from '../declarations/minepro_staking_backend/minepro_staking_backend.did';
 import { Principal } from '@dfinity/principal';
 import { AuthClient } from '@dfinity/auth-client';
 import { Actor, ActorSubclass, HttpAgent, Identity } from '@dfinity/agent';
 import { token as defaultTokenClient, createActor as createTokenActor } from '../declarations/icrc1_token/';
 import { _SERVICE as _ICRC1_TOKEN_SERVICE } from '../declarations/icrc1_token/token.did';
 
-interface Actors {
-  backend: ActorSubclass<_BACKEND_SERVICE> | undefined
-  token: ActorSubclass<_ICRC1_TOKEN_SERVICE> | undefined
-  reward: ActorSubclass<_ICRC1_TOKEN_SERVICE> | undefined
+interface UserInfo {
+  balance: bigint
+  reward: bigint
 }
 
 function App() {
-  const [actors, setActors] = useState<Actors>({backend: undefined, token: undefined, reward: undefined});
-  
+  const [identity, setIdentity] = useState<Identity | undefined>(undefined);
+
+  const [metadata, setMetadata] = useState<Metadata | undefined>(undefined);
+  const [userInfo, setUserInfo] = useState<UserInfo | undefined>(undefined);
+
   const [stakeAmount, setStakeAmount] = useState<string>('');
   const [withdrawAmount, setWithdrawAmount] = useState('');
-  const [metadata, setMetadata] = useState<Metadata>({
-    token: Principal.fromText("mwrwg-4yaaa-aaaap-qhswq-cai"),
-    reward: Principal.fromText("mwrwg-4yaaa-aaaap-qhswq-cai"),
-    fee_recipient: Principal.fromText("mwrwg-4yaaa-aaaap-qhswq-cai"),
-    leave_early_fee: BigInt(0),
-    lock_time: BigInt(0)
-  });
-
-  async function createAuthClient() {
-    const authClient = await AuthClient.create();
-
-    if (await authClient.isAuthenticated())
-      return authClient;
-
-    await new Promise((resolve, reject) => {
-      authClient.login({
-        identityProvider: "https://identity.internetcomputer.org",
-        onSuccess: resolve,
-        onError: reject,
-      })
-    });
-
-    return authClient;
-  }
 
   async function handleConnect() {
-  }
+    const authClient = await AuthClient.create();
 
-  async function fetchTokenInformation(principal: Principal) {
-    
+    if (await authClient.isAuthenticated()) {
+      setIdentity(authClient.getIdentity());
+      return;
+    }
+
+    try {
+      await new Promise((resolve, reject) => {
+        authClient.login({
+          identityProvider: "https://identity.internetcomputer.org",
+          onSuccess: resolve,
+          onError: reject,
+        })
+      });
+    } catch (e) {
+      console.error(e);
+      window.alert("Login failed");
+      return;
+    }
+
+    if (await authClient.isAuthenticated()) {
+      setIdentity(authClient.getIdentity());
+      return;
+    }
+
+    window.alert("Login failed");
   }
 
   async function tryAuth() {
     const authClient = await AuthClient.create()
-    
+
     if (!(await authClient.isAuthenticated()))
       return undefined;
 
     return authClient;
   }
 
-  async function setupActors() {
-    
-  }
-
   useEffect(() => {
-    minepro_staking_backend.getMetadata().then((mt) => {
+    const backendActor = createBackendActor(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!);
+
+    backendActor.getMetadata().then((mt) => {
       setMetadata(mt);
     });
-    
+
     tryAuth().then((authClient) => {
       if (authClient === undefined)
         return;
 
-      
+      setIdentity(authClient.getIdentity());
     });
 
   }, []);
 
+  async function fetchUserInfo() {
+    const agent = await HttpAgent.create({ identity });
+    const backendActor = createBackendActor(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!, { agent });
+
+    setUserInfo({
+      balance: await backendActor.balanceOf(identity!.getPrincipal()),
+      reward: await backendActor.pendingRewards(),
+    });
+  }
+
+  async function handleStake() {
+    if (identity === undefined) {
+      window.alert("You need to login first");
+      return;
+    }
+
+    if (metadata === undefined) {
+      return;
+    }
+
+    console.log("BLAHHH");
+
+    const amount = BigInt(stakeAmount);
+
+    const agent = await HttpAgent.create({ identity });
+    const stakedTokenActor = createTokenActor(metadata!.token, { agent });
+    const stakedTokenFee = await stakedTokenActor.icrc1_fee();
+
+    try {
+      const approveRes = await stakedTokenActor.icrc2_approve({
+        spender: {
+          owner: Principal.fromText(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!),
+          subaccount: []
+        },
+        amount: amount + stakedTokenFee,
+        fee: [],
+        memo: [],
+        from_subaccount: [],
+        created_at_time: [],
+        expected_allowance: [],
+        expires_at: []
+      });
+
+      if (approveRes.Err) {
+        window.alert("Approve token failed");
+        return;
+      }
+    } catch (e) {
+      window.alert("Approve token failed");
+      return;
+    }
+
+    const backendActor = createBackendActor(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!, { agent });
+
+    try {
+      const res: Result = await backendActor.stake(amount, []);
+
+      if (res.Err) {
+        window.alert("Stake failed, reason: " + Object.keys(res.Err)[0]);
+        return;
+      }
+    } catch (e) {
+      window.alert("Stake failed");
+      return;
+    }
+
+    window.alert("Stake success");
+    fetchUserInfo();
+  }
+
+  async function handleWithdraw() {
+
+    const amount = BigInt(withdrawAmount);
+
+    const agent = await HttpAgent.create({ identity });
+    const backendActor = createBackendActor(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!, { agent });
+
+    try {
+      const res: Result = await backendActor.withdraw(amount);
+
+      if (res.Err) {
+        window.alert("Withdraw failed, reason: " + Object.keys(res.Err)[0]);
+        return;
+      }
+    } catch (e) {
+      console.log(e);
+      window.alert("Withdraw failed");
+      return;
+    }
+
+    window.alert("Withdraw success");
+    fetchUserInfo();
+  }
+
+  async function handleClaimRewards() {
+    const agent = await HttpAgent.create({ identity });
+    const backendActor = createBackendActor(process.env.CANISTER_ID_MINEPRO_STAKING_BACKEND!, { agent });
+  
+    try {
+      const res: Result = await backendActor.claimRewards();
+
+      if (res.Err) {
+        window.alert("Claim rewards failed, reason: " + Object.keys(res.Err)[0]);
+        return;
+      }
+    } catch (e) {
+      console.log(e);
+      window.alert("Claim rewards failed");
+      return;
+    }
+
+    window.alert("Claimed rewards success");
+    fetchUserInfo();
+  }
+
+  useEffect(() => {
+    if (identity === undefined)
+      return;
+
+    fetchUserInfo();
+  }, [identity]);
+
   return (
     <main>
-      <button onClick={handleConnect}>Connect with Internet Identity</button>
+      {identity === undefined && <button onClick={handleConnect}>Connect with Internet Identity</button>}
+      {identity && <p>Logged in: {identity.getPrincipal().toString()}</p>}
       <br />
-      <label htmlFor="name">Enter amount to stake: &nbsp;</label>
+      <label htmlFor="stake">Enter amount to stake: &nbsp;</label>
       <input id="stake" alt="Stake" type="number" onChange={(e) => setStakeAmount(e.target.value)} />
-      <button onClick={() => {}}>Stake</button>
-      <section id="metadata">{
-        metadata && metadata.token.toString()
-      }</section>
+      <button onClick={() => handleStake()}>Stake</button>
+      <br />
+      <label htmlFor="withdraw">Enter amount to withdraw: &nbsp;</label>
+      <input id="withdraw" alt="Withdraw" type="number" onChange={(e) => setWithdrawAmount(e.target.value)} />
+      <button onClick={() => handleWithdraw()}>Withdraw</button>
+      <br />
+      <button onClick={() => handleClaimRewards()}>Claim Rewards</button>
+      <br />
+      <section id="metadata">
+        {metadata && <>
+          <p>Stake Token: {metadata.token.toString()}</p>
+          <p>Reward Token: {metadata.reward.toString()}</p>
+          <p>Lock Time: {metadata.lock_time.toString()}</p>
+          <p>Leave Early Fee: {metadata.leave_early_fee.toString()}</p>
+        </>
+        }
+      </section>
+
+      <section id="userinfo">
+        {userInfo && <>
+          <p>Staked Amount: {userInfo.balance.toString()}</p>
+          <p>Rewards to claim: {userInfo.reward.toString()}</p>
+        </>
+        }
+      </section>
     </main>
   );
 }
